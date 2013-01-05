909279048467d165f99a73cc202113b0b0bad94c
Matt Butcher <technosophos@gmail.com>
2013-01-05 16:51:35 -0600

    Updated Params type to also have Get method.

--- src/cookoo/params.go
+++ src/cookoo/params.go
@@ -2,8 +2,33 @@ package cookoo
 
 type Params map[string]interface{}
 
+// Check if a parameter exists, and return it if found.
 func (p Params) Has(name string) (value interface{}, ok bool) {
 	value, ok = p[name]
+	return
+}
+
+// Get a parameter value, or return the default value.
+func (p Params) Get(name string, defaultValue interface{}) interface{} {
+	val, ok := p.Has(name)
+	if ok {
+		return val
+	}
+	return defaultValue
+}
+
+// Require that a given list of parameters are present.
+// If they are all present, ok = true. Otherwise, ok = false and the 
+// `missing` array contains a list of missing params.
+func (p Params) Requires(paramNames ...string) (ok bool, missing []string) {
+	missing = make([]string, 0, len(p))
+	for _, val := range paramNames {
+		_, ok := p[val]
+		if !ok {
+			missing = append(missing, val)
+		}
+	}
+	ok = len(missing) == 0
 	return
 }
 
--- src/cookoo/params_test.go
+++ src/cookoo/params_test.go
@@ -36,4 +36,30 @@ func TestParams(t *testing.T) {
 	if _, ok := params.Validate("Test2", alwaysFails); ok {
 		t.Error("! Expected a failed validation.")
 	}
+
+	if v, ok := params.Has("Test3"); !ok {
+		t.Error("! Expected a context in Test3.")
+	} else if _, ok = v.(Context); !ok {
+		t.Error("! Expected the value to be a Context.")
+	}
+
+	if ok, missing := params.Requires("Test", "Test3"); !ok {
+		t.Error("Expected to find params. Missing ", missing)
+	}
+	
+	if ok, missing := params.Requires("Test", "Test4"); ok {
+		t.Error("! Expected to be missing something", missing)
+	} else if missing[0] != "Test4" {
+		t.Error("! Expected to be missing Test4")
+	}
+
+	get := params.Get("Test2", "NOT IT")
+	if get != "Hello" {
+		t.Error("! Expected Hello, got ", get)
+	}
+
+	get = params.Get("NotThere", "YES")
+	if get != "YES" {
+		t.Error("! Expected YES, got ", get)
+	}
 }
