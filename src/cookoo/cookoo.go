1e97acc88f1a301ac0f5ff42831f42e5b3b7f62e
Matt Butcher <technosophos@gmail.com>
2013-01-04 21:33:59 -0600

    App constructor.

--- src/cookoo/cookoo.go
+++ src/cookoo/cookoo.go
@@ -2,3 +2,16 @@ package cookoo
 
 const VERSION = "0.0.1"
 
+// Create a new Cookoo app.
+func Cookoo() (reg *Registry, router *Router, cxt Context) {
+	cxt = NewContext()
+	reg = NewRegistry()
+	router = NewRouter(reg)
+	return
+}
+
+// Execute a command and return a result.
+// A Cookoo app has a registry, which has zero or more routes. Each route
+// executes a sequence of zero or more commands. A command is of this type.
+type Command func(cxt Context, params map[string]*interface{}) interface{}
+
