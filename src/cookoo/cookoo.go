0179a23c22f8c29c48704da3b4d27721f10982aa
Matt Butcher <technosophos@gmail.com>
2013-03-21 14:54:34 -0500

    Added more package-level docs.

--- src/cookoo/cookoo.go
+++ src/cookoo/cookoo.go
@@ -2,17 +2,43 @@
 // applications.
 //
 // A chain of command framework works as follows:
-// - A "route" is constructed as a chain of commands -- a series of
-//   single-purpose tasks that are run in sequence.
-// - An application is composed of one or more routes.
-// - Commands in a route communicate using a Context.
-// - An application Router is used to receive a route name and then
-//   execute the appropriate chain of commands.
+// * A "route" is constructed as a chain of commands -- a series of
+// single-purpose tasks that are run in sequence.
+// * An application is composed of one or more routes.
+// * Commands in a route communicate using a Context.
+// * An application Router is used to receive a route name and then
+// execute the appropriate chain of commands.
 //
 // To create a new Cookoo application, use cookoo.Cookoo(). This will
 // configure and create a new registry, request router, and context.
 // From there, use the Registry to build chains of commands, and then
 // use the Router to execute chains of commands.
+//
+// Example:
+// 
+//    package main
+//
+//    import (
+//      //This is the path to Cookoo
+//      "github.com/masterminds/cookoo/src/cookoo"
+//      "fmt"
+//    )
+//
+//    func main() {
+//      // Build a new Cookoo app.
+//      registry, router, context := cookoo.Cookoo()
+//
+//      // Fill the registry.
+//      registry.Route("TEST", "A test route").Does(HelloWorld, "hi") //...
+//
+//      // Execute the route.
+//      router.HandleRequest("TEST", context, false)
+//    }
+//
+//    func HelloWorld(cxt cookoo.Context, params *cookoo.Params) interface{} {
+//      fmt.Println("Hello World")
+//      return true
+//    }
 //
 // Unlike other CoCo implementations (like Pronto.js or Fortissimo),
 // Cookoo commands are just functions.
