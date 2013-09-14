45501777c5cbeafd7fdc2769a517958261b5b660
Matt Butcher <technosophos@gmail.com>
2013-09-14 11:18:29 -0600

    Initial attempt at a prepared statement cache.

--- database/sql/datasource.go
+++ database/sql/datasource.go
@@ -21,8 +21,86 @@ import (
 // In the example above, we create a new datasource and then add it to
 // the context. This should be done at server init, before web.Serve
 // or router.HandleRequest().
-func NewDatasource(driverName, datasourceName string) (*dbsql.DB, error) {
+func NewDbDatasource(driverName, datasourceName string) (*dbsql.DB, error) {
 	return dbsql.Open(driverName, datasourceName)
 }
 
 // TODO: Prepared statement cache.
+// Create a new cache for prepared statements.
+//
+// Initial capacity determines how big the cache will be.
+//
+// Warning: The implementation of the caching layer will likely
+// change from relatively static to an LRU.
+func NewStmtCache(dbHandle *dbsql.DB, initialCapacity int) StmtCache {
+	c := new(StmtCacheMap)
+	c.cache = make(map[string]*dbsql.Stmt, initialCapacity)
+	c.capacity = initialCapacity
+	c.dbh = dbHandle
+
+	return c
+}
+
+// A StmtCache caches SQL prepared statements.
+//
+// It's intended use is as a datsource for a long-running SQL-backed
+// application. Prepared statements can exist across requests and be
+// shared by separate goroutines. For frequently executed statements,
+// this is both more performant and more secure (at least for some
+// drivers).
+//
+// IMPORTANT: Statments are cached by string key, so it is important that to
+// get the most out of the cache, you re-use the same strings. Otherwise,
+// 'SELECT surname, name FROM names' will generate a different cache entry
+// than 'SELECT name, surname FROM names'.
+//
+// The cache is driver-agnostic.
+type StmtCache interface {
+	Get(statment string) (*dbsql.Stmt, error)
+	Clear() error
+}
+
+type StmtCacheMap struct {
+	cache map[string]*dbsql.Stmt
+	capacity int
+	dbh *dbsql.DB
+}
+
+// Get a prepared statement from a SQL string.
+//
+// This will return a cached statement if one exists, otherwise
+// this will generate one, insert it into the cache, and return
+// the new statement.
+//
+// It is assumed that the underlying database layer can handle
+// parallelism with prepared statements, and we make no effort
+// to deal with locking or synchronization.
+func (c *StmtCacheMap) Get(statement string) (*dbsql.Stmt, error) {
+	if stmt, ok := c.cache[statement]; ok {
+		return stmt, nil
+	}
+	// Else we prepare the statement and then cache it.
+	stmt, err := c.dbh.Prepare(statement)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cache by string key.
+	c.cache[statement] = stmt
+
+	return stmt, nil
+}
+
+// Clear the cache.
+func (c *StmtCacheMap) Clear() error {
+	// While I don't think this is a good idea, it might be necessary. On the
+	// flip side, it might cause race conditions if one goroutine is running
+	// a query while another is clearing the cache. For now, leaving this
+	// to the memory manager.
+	//for _, stmt := range c.cache {
+	//	stmt.Close()
+	//}
+
+	c.cache = make(map[string]*dbsql.Stmt, c.capacity)
+	return nil
+}
