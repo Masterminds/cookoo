cd085943aad9110c2973c66e4cf01c7e10568e15
Matt Butcher <technosophos@gmail.com>
2013-09-14 15:58:27 -0600

    Added HTML Template command. VICTORY!

--- web/commands.go
+++ web/commands.go
@@ -1,10 +1,14 @@
 package web
 
 import (
-	//"io"
 	"github.com/masterminds/cookoo"
+	"html/template"
 	"net/http"
+	"strings"
+	"bytes"
 	"fmt"
+	"log"
+	"io"
 )
 
 // Common web-oriented commands
@@ -27,6 +31,9 @@ import (
 //
 // Note that this is optimized for writing from strings or arrays, not Readers. For larger
 // objects, you may find it more efficient to use a different command.
+//
+// Returns
+// - boolean true
 func Flush (cxt cookoo.Context, params *cookoo.Params) (interface{}, cookoo.Interrupt) {
 
 	// Make sure we have a place to write this stuff.
@@ -78,6 +85,74 @@ func Flush (cxt cookoo.Context, params *cookoo.Params) (interface{}, cookoo.Inte
 	return true, nil
 }
 
+// Render an HTML template.
+//
+// This uses the `html/template` system built into Go to render data into a writer.
+//
+// Params:
+// - template (required): A string with the full path to the template.
+// - values: An interface{} with the values to be passed to the template. If 
+//   this is not specified, the contents of the Context are passed as a map[string]interface{}.
+//   Note that datasources, in this model, are not accessible to the template.
+// - writer: The writer that data should be sent to. By default, this will create a new
+//   Buffer and put it into the context. (If no Writer was passed in, the returned writer
+//   is actually a bytes.Buffer.) To flush the contents directly to the client, you can
+//   use `.Using('writer').From('http.ResponseWriter')`.
+//
+// Returns
+// - An io.Writer. The template's contents have already been written into the writer.
+//
+// Example:
+//
+//	reg.Route("GET /html", "Test HTML").
+//		Does(cookoo.AddToContext, "_").
+//			Using("Title").WithDefault("Hello World").
+//			Using("Body").WithDefault("This is the body.").
+//		Does(web.RenderHTML, "render").
+//			Using("template").WithDefault("index.html").
+//		Does(web.Flush, "_").
+//			Using("contentType").WithDefault("text/html").
+//			Using("content").From("cxt:render")
+//
+// In the example above, we do three things:
+// - Add Title and Body to the context. For the template rendered, it will see these as
+//   {{.Title}} and {{.Body}}.
+// - Render the template located in a local file called "index.html"
+// - Flush the result out to the client. This gives you a chance to add any additional headers.
+func RenderHTML(cxt cookoo.Context, params *cookoo.Params) (interface{}, cookoo.Interrupt) {
+	ok, missing := params.Requires("template")
+	if !ok {
+		return nil, &cookoo.FatalError{"Missing params: " + strings.Join(missing, ", ")}
+	}
+	
+	var buf bytes.Buffer
+	out := params.Get("writer", &buf).(io.Writer)
+	tplName := params.Get("template", nil).(string)
+	vals := params.Get("values", cxt.AsMap())
+
+	//out := cxt.Get("http.ResponseWriter", nil).(http.ResponseWriter)
+
+	tpl, err := template.ParseFiles(tplName)
+	if err != nil {
+		return nil, &cookoo.FatalError{"Failed to parse template(s): " + tplName}
+	}
+	err = tpl.Execute(out, vals)
+	if err != nil {
+		log.Printf("Recoverable error parsing template: %s", err)
+		// XXX: This outputs partially completed templates. Is this what we want?
+		io.WriteString(out, "Template error. The error has been logged.")
+		return out, &cookoo.RecoverableError{"Template failed to completely render."}
+	}
+	return out, nil
+}
+
+// Get the server info for this request.
+//
+// This assumes that `http.Request` and `http.ResponseWriter` are in the context, which
+// they are by default.
+//
+// Returns:
+// - boolean true
 func ServerInfo(cxt cookoo.Context, params *cookoo.Params) (interface{}, cookoo.Interrupt) {
 	req := cxt.Get("http.Request", nil).(*http.Request)
 	out := cxt.Get("http.ResponseWriter", nil).(http.ResponseWriter)
