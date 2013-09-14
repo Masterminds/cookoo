6f6e09c6481c57a54523154799aadb78a731d014
Matt Butcher <technosophos@gmail.com>
2013-09-14 15:58:06 -0600

    Added AddToContext() command.

--- commands.go
+++ commands.go
@@ -14,3 +14,11 @@ func LogMessage(cxt Context, params *Params) (interface{}, Interrupt) {
 	log.Print(msg)
 	return nil, nil
 }
+
+func AddToContext(cxt Context, params *Params) (interface{}, Interrupt) {
+	p := params.AsMap()
+	for k, v := range p {
+		cxt.Add(k, v)
+	}
+	return true, nil
+}
